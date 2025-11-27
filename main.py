f96c998f9aea985b7054748f010d2ba32e90ad22
Paul-G-Bim <pgbimpro@gmail.com>
2025-11-27 14:01:14 -0400

    Create GUI window and start building layout

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from functions import get_todos, store_todos
-import FreeSimpleGUI
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter a to-do")
+add_button = sg.Button("Add")
+
+layout = [[label],
+          [input_box, add_button]]
+
+window = sg.Window("My To-do App", layout=layout)
+window.read()
+window.close()
