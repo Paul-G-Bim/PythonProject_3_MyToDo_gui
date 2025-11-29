12d217b75bc47dbbabeb2ba79419d6a2e7bba650
Paul-G-Bim <pgbimpro@gmail.com>
2025-11-29 19:22:33 -0400

    Update user experience

--- main.py
+++ main.py
@@ -31,21 +31,28 @@ while True:
     match event:
         case 'Add':
             todos = get_todos()
-            todos.append(values['todo'] + '\n')
-            store_todos(todos)
-            window['todos'].update(values=todos)
+            new_todo_text = values['todo'].strip()
+            if new_todo_text:
+                todos.append(new_todo_text + '\n')
+                store_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
 
         case 'Edit':
             try:
-                todo_to_edit = values['todos'][0]
-                new_todo = values['todo']
+                todo_to_edit_with_newline = values['todos'][0]
+                new_todo_text = values['todo'].strip()
                 todos = get_todos()
-                index = todos.index(todo_to_edit)
-                todos[index] = new_todo + '\n'
+                index = todos.index(todo_to_edit_with_newline)
+                todos[index] = new_todo_text + '\n'
                 store_todos(todos)
                 window['todos'].update(values=todos)
+                window['todo'].update(value='')
+                window['todos'].update(set_to_index=[])
             except IndexError:
                 sg.popup("Please select a to-do")
+            except ValueError:
+                sg.popup("Error: Selected item not found in the list for editing. Please try again.")
 
         case 'Delete':
             try:
@@ -55,6 +62,7 @@ while True:
                 store_todos(todos)
                 window['todos'].update(values=todos)
                 window['todo'].update(value='')
+                window['todos'].update(set_to_index=[])
             except IndexError:
                 sg.popup("Please select a to-do")
 
@@ -62,7 +70,7 @@ while True:
             break
 
         case 'todos':
-            window['todo'].update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0].strip())
 
         case sg.WIN_CLOSED:
             break
